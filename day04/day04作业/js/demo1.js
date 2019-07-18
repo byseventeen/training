974a42c0ef091ed17dbf164cb95228590d728993
seventeen luo <37255568+byseventeen@users.noreply.github.com>
2019-07-18 20:11:37 +0800

    day04

---
+++ day04/day04作业/js/demo1.js
@@ -0,0 +1,37 @@
+
+function setText1(){
+    var parent= document.getElementById("one");
+    if(document.getElementById("check1").checked==true){
+        var textElement = document.createElement("p");
+        textElement.textContent = "结点方法处理隐藏内容";
+        textElement.id="blue";   
+        //parent.appendChild(textElement);
+        parent.appendChild(textElement);
+    }
+    else{
+         var child=document.getElementById("blue");
+        parent.removeChild(child);
+    }
+}
+
+function setText2(obj){
+    if (obj.checked) {
+        document.getElementById("aa").style.display="";
+    }
+    else{
+        document.getElementById("aa").style.display="none";
+    }  
+}
+
+function insert(){
+   var parent= document.getElementById("twoul");
+   var li_1 = document.createElement("li");
+   var tt= document.getElementById("tt").value;
+   li_1.textContent = tt;  
+   parent.appendChild( li_1);
+   document.getElementById("tt").value="";
+
+}
+
+
+
