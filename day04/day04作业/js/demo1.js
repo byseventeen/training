e06d92966c3fbcf0a2b6a0619a3c9ca77a530494
seventeen luo <37255568+byseventeen@users.noreply.github.com>
2019-07-19 00:34:14 +0800

    day04

--- day04/day04作业/js/demo1.js
+++ day04/day04作业/js/demo1.js
@@ -70,7 +70,6 @@ function selectCity(selectNode) {
     citySelectNode.options.length = 1;
     // 获取选中选项的索引
     var index = selectNode.selectedIndex;
-    //alert(index);
     // 根据索引获取城市
     var cityData = citys[index];
     // 遍历城市
@@ -86,7 +85,8 @@ function selectCity(selectNode) {
 
 }
 
-function check() {
+
+function checkUserName(){
     // 获取用户输入
     var userName = document.getElementById("userName").value;
     // 验证用户输入（非空、长度不能小于5位、必须是英文或数字组成）
@@ -95,31 +95,81 @@ function check() {
     var isValid = regex.test(userName);
     if (!isValid) {
         document.getElementById("userNameMsg").textContent = "X 用户名格式不正确";
+        return name1=false;
     } else {
         document.getElementById("userNameMsg").textContent = "√";
+        return name1=true;
     }
+}
 
+function checkPassWord() {
     var pwd = document.getElementById("pwd").value;
     //至少8-16个字符，至少1个大写字母，1个小写字母和1个数字，其他可以是任意字符
     var pPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[^]{8,16}$/;
     var isValid = pPattern.test(pwd);
     if (!isValid) {
         document.getElementById("pwdMsg").textContent = "X 密码强度不足";
+        return password1=false;
     } else {
         document.getElementById("pwdMsg").textContent = "√";
+        return password1=true;
     }
+}
 
+function checkRePassWord() {
     var pwdd = document.getElementById("pwdd").value;
     if (pwd==pwdd) {
         document.getElementById("pwddMsg").textContent = "√";
+        return repassword1=true;
     }
     else{
         document.getElementById("pwddMsg").textContent = "X 密码不一致";
+        return repassword1=false;
     }
+}
 
-    
+function checkRadio() {
+    var radio1=document.getElementsById("vip");
+    var radio2=document.getElementsById("normal");
+    if(radio1.checked==true||radio2.checked==true){
+        return flag=true;
+    }else{
+        return flag=false;
+    }
+}
 
-    //return true;
+function check() {
+    checkUserName();
+    checkPassWord();
+    checkRePassWord();
+    checkRadio();
+    var cityIndex = document.getElementById("city").options.length;
+    if (name1 == true) {
+        if (password1 == true) {
+            if (repassword1 == true) {
+                if (flag) {
+                    if (cityIndex == 1) {
+                        alert("城市类型必须选择");
+                    } else {
+                        alert("提交成功");
+                    }
+                } else {
+                    alert("用户类型必须选择");
+                }
+            } else {
+                alert("X 密码不一致");
+            }
+        } else {
+            alert("X 密码不一致");
+        }
+    } else {
+        alert("用户名格式不正确");
+    }
+    return true;
+}
+
+function clickReset() {
+    location.href = "index.html";
 }
 
 
