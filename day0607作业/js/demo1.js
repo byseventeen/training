266adb48aa83da4356eba053dc9e7a7743c647dc
seventeen luo <37255568+byseventeen@users.noreply.github.com>
2019-07-22 01:20:16 +0800

    day0607

--- day0607作业/js/demo1.js
+++ day0607作业/js/demo1.js
@@ -1,4 +1,20 @@
 $(".two").click(function() {
         $(".one").show();
 });
+
+$(".down").click(function() {
+    $(".down").hide();
+    $(".listul2").show();
+    $(".up").show();
+});
+$(".up").click(function() {
+    $(".up").hide();
+    $(".listul2").hide();
+    $(".down").show();
+});
+
+$(document).ready(function(){
  
+  $(".up").hide();
+ 
+});
